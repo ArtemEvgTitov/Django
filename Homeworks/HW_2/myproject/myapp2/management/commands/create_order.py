c8f1842d7314868078cd29c9edb16449f73a5bbe
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-12-10 16:10:17 +0300

    fix

--- Homeworks/HW_2/myproject/myapp2/management/commands/create_order.py
+++ Homeworks/HW_2/myproject/myapp2/management/commands/create_order.py
@@ -7,18 +7,26 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('user_id', type=int, help='User ID')
-        parser.add_argument('product_id', type=int, help='Product ID')
+        parser.add_argument('product_id', type=int, nargs='+', help='Product ID')
 
     def handle(self, *args, **kwargs):
-        user_id = kwargs.get('user_id')
-        product_id = kwargs.get('product_id')
+        user_id = kwargs['user_id']
+        product_id = kwargs['product_id']
 
         user = User.objects.filter(pk=user_id).first()
-        product = Product.objects.filter(pk=product_id).first()
+        products = Product.objects.filter(pk__in=product_id)
+        total_price = sum(product.price for product in products)
 
-        if user and product:
-            order = Order(customer=user, product=product, total_price=450.00)
+        if user and products:
+            order = Order.objects.create(
+                customer=user,
+                total_price=total_price
+            )
+            order.products.set(products)
             order.save()
             self.stdout.write(f'Order created: {order}')
         else:
-            self.stdout.write('User or Product not found. Order creation failed.')
+            self.stdout.write(f'User or Product not found. Order creation failed. '
+                              f'\nUser: {user}'
+                              f'\nProduct: {products}'
+                              f'\nTotal price: {total_price}')
